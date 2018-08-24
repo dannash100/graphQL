7eb2e464f7ae68ffc1d6aa2cb5b6029557202804
dannash100 <danster15@gmail.com>
2018-08-24 15:54:06 +1200

    swapped rli for http

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { graphql } = require('graphql')
-const readline = require('readline')
-
 const mySchema = require('./schema/main')
-
 const { MongoClient } = require('mongodb')
 const assert = require ('assert')
+
+const graphqlHTTP = require('express-graphql')
+const express = require('express')
+const app = express()
+
 
 MONGO_URL = 'mongodb://localhost:27017/test'
 
@@ -12,18 +13,14 @@ MongoClient.connect(MONGO_URL, (err, db) => {
     assert.equal(null, err)
     console.log('Connected to MongoDB server')
 
-    const rli = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-    
-    rli.question('Client Request: ', inputQuery => {
-        // pass reference to db object to graphql(), third argument is the rootValue, which gets passed as first argument to resolver function on top level type
-        graphql(mySchema, inputQuery, {}, { db }).then(result => {
-            console.log('Server Answer:', result.data)
-            db.close(() => rli.close())
-        })
-    })
+    app.use('/graphql', graphqlHTTP({
+        schema: mySchema,
+        context: { db }
+    }))
+
+    app.listen(3000, () => 
+        console.log('running express.js on port 300')
+    )
 })
 
 
